8f04bfbdc10baf3b1dd495afecdc5cef08b25697
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2024-01-24 07:15:11 +0600

    ready to search

--- js/index.js
+++ js/index.js
@@ -16,5 +16,18 @@ const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
         console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top w-75" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0 , 200)}</p>
+            </div>
+         </div>
+        `;
+        searchResult.appendChild(div);
+
     })
 }
