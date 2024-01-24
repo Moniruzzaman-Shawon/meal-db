0b4fedceac2b3e694dce894bd65a9b3a73db43e8
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2024-01-24 06:55:18 +0600

    DB added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
 
     searchField.value = '';
 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals));
+        .then(data => displaySearchResult(data.meals));
 
-    
+}
 
-
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        console.log(meal);
+    })
 }
